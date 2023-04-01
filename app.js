70d439117694f3930262682a813e7ea409347026
Margarita <m.novolokova@gmail.com>
2023-04-02 01:11:33 +0300

    Add error handlers, config sequelize

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const express = require('express');
-const { ValidationError } = require('sequelize');
+const { errorValidateHandle, errorHandle } = require('./middlewares/error.handlers');
 const router = require('./routes');
+
 const app = express();
+
 app.use(express.static('public'))// можливість працювати з статичними файлами (з папки public)
 app.use(express.json()); // Content-Type: application/json - если не пропишем в requests то этот метод не будет работать не сможет читать
 
 //http://localhost:3000/api
 app.use('/api', router);
 
-app.use((err, req, res, next) => {
-  if (err instanceof ValidationError) {
-    res.status(400).send({
-      errors: [{ title: err.message }],
-    });
-  }
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  res.status(status).send({
-    errors: [{ title: err.message || 'Server error!!!'}],
-  });
-});
+app.use(errorValidateHandle);
+app.use(errorHandle);
 
 module.exports = app;
